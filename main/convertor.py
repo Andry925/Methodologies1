1814b8132d64b7a3f504993a1585fe7577be5378
andrew <andry925@ukr.net>
2024-04-02 14:31:26 +0300

    fix: add condition when output file is given

--- main/convertor.py
+++ main/convertor.py
@@ -77,7 +77,8 @@ class HtmlConvertor:
         for html in self.convert_paragraphs(processed_lines):
             if self.html_file:
                 self.write_to_html_file(self.html_file, html)
-            print(html)
+            else:
+                print(html)
 
 
 if __name__ == "__main__":
